f11233394013fca14b6e7194bfd80fbe462c95f2
termlt <75373156+termlt@users.noreply.github.com>
2021-08-08 23:08:09 +0400

    Update rbwish.py

--- rbwish/rbwish.py
+++ rbwish/rbwish.py
@@ -3,14 +3,11 @@ NOTE: If you got a wish message with some letters: XX or YY or Z
 That means that you need to replace them with some age info of the person you are sending this message to.
 '''
 
-
 import requests
 from bs4 import BeautifulSoup
 
 def wish():
-
 	r = requests.get('https://www.birthdaymessages.com/random/')
-
 	soup = BeautifulSoup(r.content, 'html.parser')
 
 	#Getting the WISH text
@@ -21,10 +18,8 @@ def wish():
 		print('Sorry, some error occurred. Try again!')
 
 	else:	
-
 		split = div.split('Category:', 1)[0]
 		replace = split.replace(';', ',')
 		wish = replace.replace('-', ' -')
-
 
 		print(wish)
